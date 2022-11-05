126949b08c6bca7f1ce548d3a9fdb0189fbe40f1
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-05 16:56:17 +0200

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -22,13 +22,9 @@ export default function IndexPage() {
        
        <p>
         The login can now switch To <a href={isAmp ? '/dog?amp=1' : '/dog'}>My News</a>
-        Mark territory damn that dog , but step on your keyboard while you're gaming
-        and then turn in a circle put butt in owner's face human give me
-        attention meow or eat and than sleep on your face. Friends are not food
-        jump around on couch, meow constantly until given food, or walk on car
-        leaving trail of paw prints on hood and windshield, and spread kitty
-        litter all over house, going to catch the red dot today going to catch
-        the red dot today. Jump off balcony, onto stranger's head.
+       </p>
+       <p> 
+        Jump off balcony, onto stranger's head.
       </p>
       
       
@@ -42,7 +38,7 @@ export default function IndexPage() {
           color : red
         }
         p {
-          font-size: 24px;
+          font-size: 14px;
           line-height: 30px;
           margin-top: 30px;
           color: black
