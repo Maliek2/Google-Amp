0aa3a9cbca086e07bdddd88d6deb410e94c3db86
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-05 16:07:48 +0200

    Create indexx.tsx

---
+++ pages/indexx.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import { useAmp } from 'next/amp'
+import Layout from '../components/Layout'
+import Byline from '../components/Byline'
+
+export const config = {
+  amp: true,
+}
+
+export default function IndexPage() {
+  const isAmp = useAmp()
+
+  return (
+
+    <Layout>
+      <Head>
+        <title>The Mobile News</title>
+
+
+      </Head>
