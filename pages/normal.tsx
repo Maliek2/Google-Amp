a95bdc837d84cf5546c078aa36d25c82ae11f56f
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-05 16:15:20 +0200

    Update normal.tsx

--- pages/normal.tsx
+++ pages/normal.tsx
@@ -1,3 +1,15 @@
 export default function NormalPage() {
-  return <p>I'm just a normal old page, no AMP for me</p>
+  return 
+<p>
+When a component suspends, it activates the fallback of only the nearest parent Suspense boundary. This means you can nest multiple Suspense boundaries to create a loading sequence. Each Suspense boundary’s fallback will be filled in as the next level of content becomes available.
+
+To illustrate, consider the following example:
+
+
+
+I'm just a normal old page, no AMP for me
+
+
+
+</p>
 }
