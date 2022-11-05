d483edbc268b27eabe75ea17aabb3290959daa36
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-05 16:09:40 +0200

    Update indexx.tsx

--- pages/indexx.tsx
+++ pages/indexx.tsx
@@ -18,3 +18,53 @@ export default function IndexPage() {
 
 
       </Head>
+       <p>
+        The Login Users  Can Now Switch To <a href={isAmp ? '/dog?amp=1' : '/dog'}>Mobile Users</a>,
+        Sit and stare if it fits, i sits. Mark
+        territory damn that dog , but step on your keyboard while you're gaming
+        and then turn in a circle put butt in owner's face human give me
+        attention meow or eat and than sleep on your face. Friends are not food
+        jump around on couch, meow constantly until given food, or walk on car
+        leaving trail of paw prints on hood and windshield, and spread kitty
+        litter all over house, going to catch the red dot today going to catch
+        the red dot today. Jump off balcony, onto stranger's head.
+      </p>
+      
+      
+      <p>
+        Ignore the squirrels, you'll never catch them anyway missing until
+        dinner time, for intrigued by the shower, so i could pee on this if i
+        had the energy for purrrrrr for vommit food and eat it again lick butt
+        and make a weird face. Rub whiskers on bare skin act innocent eat grass,
+        throw it back up or lick yarn hanging out of own butt. I am the best cat
+        is love, cat is life, or sleep nap, mew but meoooow!. Meowzer!. Friends
+        are not food jump off balcony, onto stranger's head intrigued by the
+        shower, and eat a plant, kill a hand, touch water with paw then recoil
+        in horror yet flop over.
+      </p>
+      
+      <style jsx>
+           {`
+        h1 {
+          margin-bottom: 5px;
+        }
+        p {
+          font-size: 20px;
+          line-height: 30px;
+          margin-top: 30px;
+        }
+        .caption {
+          color: #ccc;
+          margin-top: 0;
+          font-size: 14px;
+          text-align: center;
+        }
+      `}
+
+</style>
+
+
+
+    </Layout>
+  )
+}
