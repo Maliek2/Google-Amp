221a8a18d6b77659cb99d36fb8cba93dd02f249f
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-05 16:05:36 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ export default function IndexPage() {
   const isAmp = useAmp()
 
   return (
+
     <Layout>
       <Head>
         <title>The Mobile News</title>
@@ -19,7 +20,7 @@ export default function IndexPage() {
       </Head>
 
 
-      <h1>The Mobile News</h1>
+      <h1>The Maliek News </h1>
       <p className="caption">Maliek Galant</p>
 
       <Byline author="Login Users" />
@@ -31,7 +32,7 @@ export default function IndexPage() {
         width="550"
         height="368"
         layout="responsive"
-        src="https://programmablesearchengine.google.com/about/mod1_image_xl_notext_2x.webp"
+        src="https://avatars.githubusercontent.com/u/14214240?s=200&v=4"
       >
         <amp-img
           alt="Mountains"
@@ -39,39 +40,14 @@ export default function IndexPage() {
           width="550"
           height="368"
           layout="responsive"
-          src="https://programmablesearchengine.google.com/about/mod3_generic_phone_xl_2x.webp"
+          src="  "
         ></amp-img>
       </amp-img>
 
 
       <p>
         The Login Users  Can Now Switch To <a href={isAmp ? '/dog?amp=1' : '/dog'}>Mobile Users</a>,
-        eat grass, throw it back up but refuse to leave cardboard box or groom
-        yourself 4 hours - checked, have your beauty sleep 18 hours - checked,
-        be fabulous for the rest of the day - checked!. Hide from vacuum
-        cleaner. Chirp at birds chew foot chase the pig around the house and
-        meoooow!. Chase ball of string climb a tree, wait for a fireman jump to
-        fireman then scratch his face claw drapes, for meow to be let in yet
-        attack dog, run away and pretend to be victim meow loudly just to annoy
-        owners. Touch water with paw then recoil in horror hide when guests come
-        over, and tuxedo cats always looking dapper so has closed eyes but still
-        sees you or a nice warm laptop for me to sit on pee in human's bed until
-        he cleans the litter box. Steal the warm chair right after you get up
-        cat not kitten around yet claws in your leg eat all the power cords.
-        Lick sellotape curl into a furry donut immediately regret falling into
-        bathtub or you call this cat food? and fall asleep on the washing
-        machine. Purr for no reason hack up furballs and pelt around the house
-        and up and down stairs chasing phantoms. If it smells like fish eat as
-        much as you wish. Unwrap toilet paper chew iPad power cord white cat
-        sleeps on a black shirt lick the other cats. Lounge in doorway mew. Has
-        closed eyes but still sees you sleep on keyboard, so hunt anything that
-        moves lick sellotape but slap owner's face at 5am until human fills food
-        dish if it smells like fish eat as much as you wish. Meow to be let in
-        find empty spot in cupboard and sleep all day and thug cat sit by the
-        fire burrow under covers always hungry. Swat at dog hide when guests
-        come over purrrrrr chew on cable so mark territory, yet howl on top of
-        tall thing or find something else more interesting. Chase mice kitten is
-        playing with dead mouse. Sit and stare if it fits, i sits. Mark
+        Sit and stare if it fits, i sits. Mark
         territory damn that dog , but step on your keyboard while you're gaming
         and then turn in a circle put butt in owner's face human give me
         attention meow or eat and than sleep on your face. Friends are not food
