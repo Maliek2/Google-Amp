2b98930c21ac257c94e251001c67cad1b410e77f
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-07 00:00:28 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,15 +47,8 @@ export default function IndexPage() {
       <!-- We will put our React component inside this div. -->
       <div class="like_button_container" data-commentid="3"></div>
     </p>
-    <div id="root"></div>
-    <!-- Load React. -->
-  <!-- Note: when deploying, replace "development.js" with "production.min.js". -->
-  <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
-  <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
-
-  <!-- Load our React component. -->
-  <script src="like_button.js"></script>
-
+    
+  
 
       <style jsx>
     * {
