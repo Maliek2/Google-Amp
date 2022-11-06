ccb19cff8715154da02da843af3ac952a5f94e11
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-07 00:04:09 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,27 +26,9 @@ export default function IndexPage() {
        <p> 
         Jump off balcony, onto stranger's head.
       </p>
-      <h2>One Minute</h2>
-    <p>This page demonstrates using React with no build tooling.</p>
-    <p>React is loaded as a script tag.</p>
+      
+      <p>React is loaded as a script tag.</p>
 
-    <p>
-      This is the first comment.
-      <!-- We will put our React component inside this div. -->
-      <div class="like_button_container" data-commentid="1"></div>
-    </p>
-
-    <p>
-      This is the second comment.
-      <!-- We will put our React component inside this div. -->
-      <div class="like_button_container" data-commentid="2"></div>
-    </p>
-
-    <p>
-      This is the third comment.
-      <!-- We will put our React component inside this div. -->
-      <div class="like_button_container" data-commentid="3"></div>
-    </p>
     
   
 
