3cef901c2e2b58541f8102a9a8d48d37a07093e9
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-06 23:23:27 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -25,3 +25,17 @@ class Component extends React.Component<PropsType, {}> {
   <h1>Hello</h1>
   World
 </Component>
+
+<p>
+        The login can now switch To <a href={isAmp ? '/index?amp=1' : '/index'}>Mobile</a>,
+        Sit and stare if it fits, i sits. Mark <a href={isAmp ? '/index?amp=1' : '/index'}>Best News</a>
+        territory damn that dog , but step on your keyboard while you're gaming
+        and then turn in a circle put butt in owner's face human give me
+        attention meow or eat and than sleep on your face. Friends are not food
+        jump around on couch, meow constantly until given food, or walk on car
+        leaving trail of paw prints on hood and windshield, and spread kitty
+        litter all over house, going to catch the red dot today going to catch
+        the red dot today. Jump off balcony, onto stranger's head.
+        <a href={isAmp ? '/index?amp=1' : '/index'}>My Blog</a>
+      
+</p>
