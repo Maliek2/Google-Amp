5b67948099a5757b33d38e752d6752eda3750c49
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-08 13:45:40 +0200

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+import { useAmp } from 'next/amp'
+import Layout from '../components/Layout'
+import Byline from '../components/Byline'
+
+export const config = {
+  amp: true,
+}
+
+export default function IndexPage() {
+  const isAmp = useAmp()
+
+  return (
+
+    <Layout>
+      <Head>
+        <title>The Mobile Login</title>
+
+
+      </Head>
+       <h1>The Maliek News </h1>
+       
+       <p>
+        The login can now switch To <a href={isAmp ? '/dog?amp=1' : '/dog'}>My News</a>
+       </p>
+       <p> 
+        Jump off balcony, onto stranger's head.
+      </p>
+      <p>
+        Ice cream lollipop marshmallow tiramisu jujubes croissant. Bear claw
+        lemon drops marzipan candy bonbon cupcake powder. Candy canes cheesecake
+        bear claw pastry cake donut jujubes. Icing tart jelly-o soufflé bonbon
+        apple pie. Cheesecake pie chupa chups toffee powder. Bonbon lemon drops
+        carrot cake pudding candy halvah cheesecake lollipop cupcake. Pudding
+        marshmallow fruitcake. Gummi bears bonbon chupa chups lemon drops. Wafer
+        dessert gummies gummi bears biscuit donut tiramisu gummi bears brownie.
+        Tootsie roll liquorice bonbon cookie. Sesame snaps chocolate bar cake
+        croissant chupa chups cheesecake gingerbread tiramisu jelly. Cheesecake
+        ice cream muffin lollipop gummies. Sesame snaps jelly beans sweet bear
+        claw tart.
+      </p>
+
+
+
+       </div> 
+      )
+}
+
